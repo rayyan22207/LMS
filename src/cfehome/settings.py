9f80e49290088a7ce32a57be73ada79663a66d47
rayyan22207 <rayyan22207@gmail.com>
2025-08-03 23:02:03 +0500

    working on

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -250,3 +250,5 @@ STORAGES = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'accounts.User'
