95ef2c5f01626b9bd7f019d98426999a3f581aef
rayyan22207 <rayyan22207@gmail.com>
2025-08-04 15:27:55 +0500

    working on

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
-# Register your models here.
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name', 'email', 'is_instructor', 'is_student', 'is_staff')
+    list_filter = ('is_instructor', 'is_student', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('id',)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email', 'bio', 'avatar')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_instructor', 'is_student', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Social Links', {'fields': ('linkedin_url', 'twitter_url')}),
+    )
