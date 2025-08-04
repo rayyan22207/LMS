a3dee9a924e5a885361588931e054dbb50cc2012
rayyan22207 <rayyan22207@gmail.com>
2025-08-04 16:06:38 +0500

    working on

--- src/courses/views.py
+++ src/courses/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Course
 
-# Create your views here.
+class CourseListView(ListView):
+    model = Course
+    template_name = 'courses/course_list.html'
+    context_object_name = 'courses'
+    queryset = Course.objects.filter(is_published=True).select_related('instructor', 'category')
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course_detail.html'
+    context_object_name = 'course'
+
+    def get_queryset(self):
+        return Course.objects.select_related('instructor', 'category').prefetch_related('modules__lessons')
