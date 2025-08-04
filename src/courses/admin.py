bd407ac4935e26fb81d3bc40983d2ad8124d9889
rayyan22207 <rayyan22207@gmail.com>
2025-08-04 14:28:34 +0500

    admin setup

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Category, Course, Module, Lesson
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Course)
+admin.site.register(Module)
+admin.site.register(Lesson)
+
--- src/media/admin.py
+++ src/media/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import UploadedMedia
 
-# Register your models here.
+admin.site.register(UploadedMedia)
